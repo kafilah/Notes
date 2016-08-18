28c6a17e6dbf5b8ac107814f4737c827011fa3a1
Kafilah muhammad <kalimuhammad@wesleyan.edu>
2016-08-18 15:07:38 -0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Kafilah on 8/18/16.
+//  Copyright © 2016 Kafilah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
